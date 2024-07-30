c1be08599b50bd26833c0d177a8532cc0f46c3c3
aimathstats <aimathstats@gmail.com>
2024-07-30 15:00:12 +0900

    Update pawpatAI.py

--- pawpatAI.py
+++ pawpatAI.py
@@ -13,6 +13,10 @@ voice = "alloy"
 
 from audio_recorder_streamlit import audio_recorder
 audio_bytes = audio_recorder()
+if st.button("Save Recording"):
+    with open("recorded_audio.wav", "wb") as f:
+        f.write(audio_bytes)
+    st.success("Recording saved!")
 
 #########################################
 #user_input = st.text_area("テキストを入力してください", "Hello, Paw patrol!", height=200)
