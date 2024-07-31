99a295d17ca6cf7266079c895f9f6c0b23b870bf
aimathstats <aimathstats@gmail.com>
2024-07-31 09:53:30 +0900

    Update pawpatAI.py

--- pawpatAI.py
+++ pawpatAI.py
@@ -11,6 +11,33 @@ st.subheader("パウパトについて何でも聞いてみよう！")
 character = "ケント"
 voice = "alloy"
 
+
+###
+def write_audio_file(file_path, audio_bytes):
+    with open(file_path, "wb") as audio_file:
+        audio_file.write(audio_bytes)
+
+audio_bytes = audio_recorder(
+    text="click and speak>>>",
+    recording_color="#e8b62c",
+    neutral_color="#6aa36f",
+    icon_name="microphone-lines",
+    icon_size="3x",
+    pause_threshold=2.0,
+    sample_rate=41_000
+)  
+
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+    write_audio_file("recorded_audio.wav", audio_bytes)
+
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=open("recorded_audio.wav", "rb"),
+    )
+    st.text(transcript.text)
+
+
 ######################################
 # 音声入力
 #from audio_recorder_streamlit import audio_recorder
@@ -34,13 +61,13 @@ voice = "alloy"
 
 ########################################
 # 音声入力
-from audio_recorder_streamlit import audio_recorder
-audio_bytes = audio_recorder()
-#if st.button("Save Recording"):
-#    with open("speech.wav", "wb") as f:
-#        f.write(audio_bytes)
-#    st.success("Recording saved!")
-st.audio(audio_bytes)
+#from audio_recorder_streamlit import audio_recorder
+#audio_bytes = audio_recorder()
+##if st.button("Save Recording"):
+##    with open("speech.wav", "wb") as f:
+##        f.write(audio_bytes)
+##    st.success("Recording saved!")
+#st.audio(audio_bytes)
 
 # 音声認識
 #audio_file2 = open("speech.wav", "rb")
