4e29ae20774ca75b41db824c40d42b4a72da7bfb
aimathstats <aimathstats@gmail.com>
2024-07-31 12:33:17 +0900

    Update pawpatAI.py

--- pawpatAI.py
+++ pawpatAI.py
@@ -191,7 +191,7 @@ if input_text:
                 for m in st.session_state.messages
             ],
             stream = True,
-            temperature = 0.5,
+            temperature = st.session_state.temperature,
         )
         # AIの返答を流れるように出力
         response = st.write_stream(stream)
