61b5385b5958fedcc59afbc59e60a364d31b196c
aimathstats <aimathstats@gmail.com>
2024-08-01 15:41:14 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -34,12 +34,12 @@ for message in st.session_state.messages[1:]:
         st.markdown(message["content"])
 
 prompt = "こんにちは！"
-endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(30))
 
 st.session_state.messages.append({"role": "user", "content": prompt})
 with st.chat_message("user"):
     st.markdown(prompt)
 
+endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(30))
 while datetime.datetime.now() < endtime:
     #time.sleep(1)
     with st.chat_message("assistant"):
