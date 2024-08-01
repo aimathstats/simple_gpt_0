1ae0d103d67df0e8bb2aa3fef16a88ea3c8b51a2
aimathstats <aimathstats@gmail.com>
2024-08-01 14:07:35 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 import streamlit as st
+import datetime
+import time
 
 st.title("ChatGPT-like clone")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
@@ -14,21 +16,23 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("What is up?")
-
-if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(10))
+while datetime.datetime.now() < endtime:
+    time.sleep(1)
+    prompt = st.chat_input("続きは？")
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+    
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
