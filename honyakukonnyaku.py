45204a6b437636c11c7109cc82e0f572cca73762
aimathstats <aimathstats@gmail.com>
2024-08-01 12:07:07 +0900

    Update honyakukonnyaku.py

--- honyakukonnyaku.py
+++ honyakukonnyaku.py
@@ -7,7 +7,8 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 st.set_page_config(page_title="honyaku konnyaku", page_icon=":material/globe_asia:")
 st.title("ほんやくコンニャク")
-lang = st.radio("", ["英語", "フランス語", "中国語"], horizontal = True)
+#lang = st.radio("", ["英語", "フランス語", "中国語"], horizontal = True)
+lang = st.radio(["英語", "フランス語", "中国語"], horizontal = True)
 voice = "alloy"
 
 def write_audio_file(file_path, audio_bytes):
