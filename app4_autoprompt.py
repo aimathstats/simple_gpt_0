c34ec1c7e385fd2f86d0a025fff3aa15c70af919
aimathstats <aimathstats@gmail.com>
2024-08-01 15:22:36 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -16,11 +16,6 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
-# new codes
-if "messages2" not in st.session_state:
-    st.session_state.messages2 = []
-
 template = '''
 あなたは質問者です。相手は仲のよい友人ですので、挨拶は不要です。
 相手の返答に対して、必ず何か質問してください。
@@ -29,55 +24,39 @@ template = '''
 相手は仲のよい友人ですので挨拶は不要です。
 '''
 
-#prompt = st.chat_input("質問？")
 prompt = "こんにちわ"
-response = False
-
-#endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(10))
-#while datetime.datetime.now() < endtime:
-#    time.sleep(2)
-#    #prompt = "こんにちわ"
+endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(10))
 
 st.session_state.messages.append({"role": "user", "content": prompt})
 with st.chat_message("user"):
     st.markdown(prompt)
 
-with st.chat_message("assistant"):
-    stream = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-        temperature = 0.5,
-    )
-    response = st.write_stream(stream)
-st.session_state.messages.append({"role": "assistant", "content": response})
+while datetime.datetime.now() < endtime:
+    time.sleep(1)
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+            temperature = 0.5,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
-with st.chat_message("user"):
-    stream2 = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": "system", "content": template},
-            {"role": "user", "content": response}
-        ],
-        stream=True,
-        temperature = 0.5,
-    )
-    prompt = st.write_stream(stream2)
-st.session_state.messages.append({"role": "user", "content": prompt})
-
-with st.chat_message("assistant"):
-    stream = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-        temperature = 0.5,
-    )
-    response = st.write_stream(stream)
-st.session_state.messages.append({"role": "assistant", "content": response})
+    time.sleep(1)
+    with st.chat_message("user"):
+        stream2 = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "system", "content": template},
+                {"role": "user", "content": response}
+            ],
+            stream=True,
+            temperature = 0.5,
+        )
+        prompt = st.write_stream(stream2)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
