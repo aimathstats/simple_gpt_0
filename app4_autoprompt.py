ecee4d100ed0db1289a331204d24a744f61c2edb
aimathstats <aimathstats@gmail.com>
2024-08-01 14:48:40 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -22,11 +22,25 @@ if "messages2" not in st.session_state:
     st.session_state.messages2 = []
 #prompt = st.chat_input("質問？")
 prompt = "こんにちわ"
+response = False
 
 endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(10))
 while datetime.datetime.now() < endtime:
     time.sleep(2)
     #prompt = "こんにちわ"
+    if response:
+        with st.chat_message("user"):
+            stream2 = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": "user", "content": response}
+                ],
+                stream=True,
+            )
+            prompt = st.write_stream(stream2)
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
@@ -44,18 +58,6 @@ while datetime.datetime.now() < endtime:
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
     
-    if response:
-        with st.chat_message("user"):
-            stream2 = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": "user", "content": response}
-                ],
-                stream=True,
-            )
-            prompt = st.write_stream(stream2)
-            st.markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
         
 
 
