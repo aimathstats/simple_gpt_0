4a55d48300c4e326115b108b9990392d1ecd03f7
aimathstats <aimathstats@gmail.com>
2024-08-01 12:16:09 +0900

    Update honyakukonnyaku.py

--- honyakukonnyaku.py
+++ honyakukonnyaku.py
@@ -108,12 +108,11 @@ if input_text:
                 {"role": "system", "content": template2},
                 {"role": "user", "content": response}
             ],
-            stream = True,
             temperature = 0.5,
         )
-        response2 = st.write_stream(stream2)
+        #response2 = st.write_stream(stream2)
         #st.markdown(response2)
-        r2 = "逆翻訳" + response2
+        r2 = "逆翻訳" + stream2
         st.markdown(r2)
         
         # 音声合成（with tts-1）
