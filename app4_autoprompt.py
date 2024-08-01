986213462e76f14ca57d7e7b030f5b1bdd093e71
aimathstats <aimathstats@gmail.com>
2024-08-01 14:43:01 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -16,15 +16,17 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-#prompt = st.chat_input("質問？")
 
+# new codes
 if "messages2" not in st.session_state:
     st.session_state.messages2 = []
+#prompt = st.chat_input("質問？")
+prompt = "こんにちわ"
 
 endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(10))
 while datetime.datetime.now() < endtime:
     time.sleep(2)
-    prompt = "こんにちわ"
+    #prompt = "こんにちわ"
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
@@ -41,3 +43,20 @@ while datetime.datetime.now() < endtime:
             )
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+    
+    if response:
+        with st.chat_message("user"):
+            stream2 = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": "user", "content": response}
+                ],
+                stream=True,
+            )
+            prompt = st.write_stream(stream2)
+            st.markdown(prompt)
+        
+
+
+
+
