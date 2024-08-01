e5ca83079e80e3ca8bfff7a09e39ebe3a0e88a30
aimathstats <aimathstats@gmail.com>
2024-08-01 15:31:31 +0900

    Update app4_autoprompt.py

--- app4_autoprompt.py
+++ app4_autoprompt.py
@@ -6,22 +6,31 @@ import time
 st.title("自動おしゃべりAI")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+template_system = '''
+あなたは質問者に対して回答する、回答AIです。
+相手の返答に対して、必ず何か回答してください。
+内容は簡潔に一文でお願いします。
+相手は仲のよい友人ですので挨拶は不要です。
+'''
+
+template_user = '''
+あなたは回答者に対して質問する、質問AIです。
+相手の返答に対して、必ず何か質問してください。
+質問内容は何でもいいですが、答えるのが簡単なものにして下さい。
+質問は簡潔に一文でお願いします。
+相手は仲のよい友人ですので挨拶は不要です。
+'''
+
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o-mini"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages = [{'role': 'system', 'content': template_system}]
 
-for message in st.session_state.messages:
+for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-template_user = '''
-あなたは質問者です。相手は仲のよい友人ですので、挨拶は不要です。
-相手の返答に対して、必ず何か質問してください。
-質問内容は何でもいいですが、一般的で答えるのが簡単なものにして下さい。
-質問は簡潔に一文でお願いします。相手は仲のよい友人ですので挨拶は不要です。
-'''
 
 prompt = "こんにちは！"
 endtime = datetime.datetime.now() + datetime.timedelta(seconds=int(30))
