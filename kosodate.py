978f259b4c9fc04f817c1292a62d0005f4407176
aimathstats <aimathstats@gmail.com>
2024-08-02 11:31:52 +0900

    Update kosodate.py

--- kosodate.py
+++ kosodate.py
@@ -1,7 +1,7 @@
-from openai import OpenAI
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+from openai import OpenAI
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
@@ -10,8 +10,6 @@ st.title("子育てアドバイスAI")
 st.subheader("『子育てで困ったら、これやってみ！』てぃ先生")
 voice = "shimmer"
 
-#######################################################
-# 音声入力と音声認識（with whisper）
 def write_audio_file(file_path, audio_bytes):
     with open(file_path, "wb") as audio_file:
         audio_file.write(audio_bytes)
@@ -27,7 +25,7 @@ def audio_to_text(audio_bytes):
 # data
 df = pd.read_csv('data/kosodate.csv')
 df = df.drop("ID", axis=1)
-data2 = df.to_string()
+data = df.to_string()
 
 # template
 template = '''
@@ -43,65 +41,45 @@ template = '''
 - 極めて短い文章で答えて。どうしてその回答になったのかの「理由」を質問されたら、続けて答えてください。
 
 ### 資料
-"""__MSG__"""
+"""__CSV__"""
 '''
-#特に、__MSG2__になりきってください。
-template = template.replace('__MSG__', data2.replace('"', ''))
-#template = template.replace('__MSG2__', character.replace('"', ''))
 
-# OpenAIのモデルを指定
+template = template.replace('__CSV__', data.replace('"', ''))
+
 if "openai_model" not in st.session_state:
-    #st.session_state["openai_model"] = "gpt-3.5-turbo"
     st.session_state["openai_model"] = "gpt-4o-mini"
 
-# チャットの履歴 messages を初期化（一つ一つの messages は {role, content} の形式）
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages = [{'role': 'system', 'content': template}]
 
-# それまでのメッセージを全て表示したままにする（このloopがないと、同じ場所を更新しながら会話が続く）
-#for message in st.session_state.messages[1:]:
-#    with st.chat_message(message["role"]):
-#        st.markdown(message["content"]) # 表示する（一瞬ですべて書き下す）
-
-# 入力（音声/テキスト）
 col1, col2 = st.columns(2)
 with col1:
     audio_bytes = audio_recorder(
-    text="", recording_color="#f21835", #recording_color="#e8b62c",
-    neutral_color="#2EF218", icon_name="microphone", icon_size="2x",
+    text="", recording_color="#f21835", neutral_color="#2EF218", icon_name="microphone", icon_size="2x",
     pause_threshold=5.0, sample_rate=41_000
 )
 with col2:
-    prompt = st.chat_input("しつもんは？")
+    prompt = st.chat_input("悩み・言い換え？")
 
-# 音声入力がある場合、テキストに変換
 if audio_bytes:
     audio_transcript = audio_to_text(audio_bytes)
     if audio_transcript:
         st.session_state.audio_transcript = audio_transcript
 
-# テキスト入力がある場合、音声入力をリセット
 if prompt and 'audio_transcript' in st.session_state:
     del st.session_state.audio_transcript
 
 input_text = st.session_state.audio_transcript if 'audio_transcript' in st.session_state else prompt
 
-# old codes
 if input_text:
-    # messagesにユーザーのプロンプトを追加
     st.session_state.messages.append({"role": "user", "content": input_text})
-
-    # ユーザーのアイコンで、promptをそのまま表示
     with st.chat_message("user"):
         st.markdown(input_text)
 
-    # AIのアイコンで
     with st.chat_message("assistant"):
-        # ChatGPTの返答をstreamに格納
         stream = client.chat.completions.create(
             model = st.session_state["openai_model"],
-            # 会話履歴をすべて参照して渡す
             messages = [
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
@@ -109,29 +87,21 @@ if input_text:
             stream = True,
             temperature = 0.5,
         )
-        # AIの返答を流れるように出力
         response = st.write_stream(stream)
         
-        # 音声合成（with tts-1）
         user_input = response
         if user_input:
             try:
-                # 音声合成リクエストの送信
                 response_audio = client.audio.speech.create(
                     model="tts-1",
                     voice=voice,
                     input=user_input,
                 )
-                # 結果をファイルに保存
                 output_file = "output.mp3"
                 response_audio.stream_to_file(output_file)
-                # ユーザに音声ファイルをダウンロードするオプションを提供
                 st.audio(output_file, autoplay = True)
             except Exception as e:
                 st.error(f"エラーが発生しました: {e}")
         else:
             st.warning("テキストを入力してください。")
-    
-    # messagesにAIの返答を格納
     st.session_state.messages.append({"role": "assistant", "content": response})
-    #　ここで一旦終わり、入力待機となる
