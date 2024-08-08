844c636a2675f28f53e11b369f45a0c722155414
aimathstats <aimathstats@gmail.com>
2024-08-09 08:45:25 +0900

    Update app5_autodatanalysis.py

--- app5_autodatanalysis.py
+++ app5_autodatanalysis.py
@@ -1,1 +1,71 @@
+from openai import OpenAI
+import streamlit as st
+import pandas as pd
 
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+st.title("自動データ分析")
+#st.subheader("統計学（前半）に関する質問に答えます")
+
+# data
+df = pd.read_csv('data/combined1.csv')
+data1 = df["説明"]
+data2 = data1.to_string()
+
+# template
+template = '''
+あなたは大学経済学部の2年次開講科目「統計学」を担当する大学教員です。
+
+### 条件
+- 全ての質問に対して、講義内容に関する以下の詳細な「授業資料」を参照した上で、正確に答えてください。
+
+### 授業資料
+"""__MSG__"""
+'''
+
+template = template.replace('__MSG__', data2.replace('"', ''))
+
+# OpenAIのモデルを指定
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+# チャットの履歴 messages を初期化（一つ一つの messages は {role, content} の形式）
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages = [{'role': 'system', 'content': template}]
+
+
+# それまでのメッセージを全て表示したままにする（このloopがないと、同じ場所を更新しながら会話が続く）
+#for message in st.session_state.messages[1:]:
+#    with st.chat_message(message["role"]):
+#        st.markdown(message["content"]) # 表示する（一瞬ですべて書き下す）
+
+
+# 入力されたら、内容をpromptに格納(入力までは待機)
+if prompt := st.chat_input("質問はありますか？"):
+    # messagesにユーザーのプロンプトを追加
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # ユーザーのアイコンで、promptをそのまま表示
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # AIのアイコンで
+    with st.chat_message("assistant"):
+        # ChatGPTの返答をstreamに格納
+        stream = client.chat.completions.create(
+            model = st.session_state["openai_model"],
+            # 会話履歴をすべて参照して渡す
+            messages = [
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream = True,
+            temperature = 0.5,
+        )
+        # AIの返答を流れるように出力
+        response = st.write_stream(stream)
+    
+    # messagesにAIの返答を格納
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    #　ここで一旦終わり、入力待機となる
