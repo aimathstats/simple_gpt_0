689f082ab423b958835aa6fded1bfbf5312af725
aimathstats <aimathstats@gmail.com>
2024-08-09 08:52:45 +0900

    Update app5_autodatanalysis.py

--- app5_autodatanalysis.py
+++ app5_autodatanalysis.py
@@ -8,38 +8,30 @@ st.title("自動データ分析")
 #st.subheader("統計学（前半）に関する質問に答えます")
 
 # data
-df = pd.read_csv('data/combined1.csv')
-data1 = df["説明"]
-data2 = data1.to_string()
+df = pd.read_csv('data/koukouseiseki.csv')
+data1 = df["国語"]
+st.write(data1)
 
 # template
 template = '''
 あなたは大学経済学部の2年次開講科目「統計学」を担当する大学教員です。
 
-### 条件
-- 全ての質問に対して、講義内容に関する以下の詳細な「授業資料」を参照した上で、正確に答えてください。
-
-### 授業資料
 """__MSG__"""
 '''
 
-template = template.replace('__MSG__', data2.replace('"', ''))
+template = template.replace('__MSG__', data1.replace('"', ''))
 
-# OpenAIのモデルを指定
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o-mini"
 
-# チャットの履歴 messages を初期化（一つ一つの messages は {role, content} の形式）
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages = [{'role': 'system', 'content': template}]
-
 
 # それまでのメッセージを全て表示したままにする（このloopがないと、同じ場所を更新しながら会話が続く）
 #for message in st.session_state.messages[1:]:
 #    with st.chat_message(message["role"]):
 #        st.markdown(message["content"]) # 表示する（一瞬ですべて書き下す）
-
 
 # 入力されたら、内容をpromptに格納(入力までは待機)
 if prompt := st.chat_input("質問はありますか？"):
