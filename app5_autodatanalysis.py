e868fab10db72cf81f127878e21e19a3348c1b22
aimathstats <aimathstats@gmail.com>
2024-08-09 09:05:51 +0900

    Update app5_autodatanalysis.py

--- app5_autodatanalysis.py
+++ app5_autodatanalysis.py
@@ -9,56 +9,68 @@ st.title("自動データ分析")
 
 # data
 df = pd.read_csv('data/koukouseiseki.csv')
-data1 = df["国語"]
-st.write(data1)
+st.write(df["国語"])
 
+# Upload a file with an "assistants" purpose
+file = client.files.create(
+  file=open("data/koukouseiseki.csv", "rb"),
+  purpose='assistants'
+)
+
+# Create an assistant using the file ID
+assistant = client.beta.assistants.create(
+  instructions="あなたはデータ分析の教員です。データと質問が与えられたら、適切なコードを書いて。",
+  model="gpt-4o",
+  tools=[{"type": "code_interpreter"}],
+  tool_resources={
+    "code_interpreter": {
+      "file_ids": [file.id]
+    }
+  }
+)
+
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": "このデータの基本統計量を教えて。すべて日本語の文章として生成して。",
+      "attachments": [
+        {
+          "file_id": file.id,
+          "tools": [{"type": "code_interpreter"}]
+        }
+      ]
+    }
+  ]
+)
+
+###################################
 # template
-template = '''
-あなたは大学経済学部の2年次開講科目「統計学」を担当する大学教員です。
+#template = '''
+#あなたは大学経済学部の2年次開講科目「統計学」を担当する大学教員です。
+#'''
 
-"""__MSG__"""
-'''
+#if "openai_model" not in st.session_state:
+#    st.session_state["openai_model"] = "gpt-4o-mini"
+#
+#if "messages" not in st.session_state:
+#    st.session_state.messages = []
+#    st.session_state.messages = [{'role': 'system', 'content': template}]
+#
+#if prompt := st.chat_input("質問はありますか？"):
+#    st.session_state.messages.append({"role": "user", "content": prompt})
+#    with st.chat_message("user"):
+#        st.markdown(prompt)
+#    with st.chat_message("assistant"):
+#        stream = client.chat.completions.create(
+#            model = st.session_state["openai_model"],
+#            messages = [
+#                {"role": m["role"], "content": m["content"]}
+#                for m in st.session_state.messages
+#            ],
+#            stream = True,
+#            temperature = 0.5,
+#        )
+#        response = st.write_stream(stream)
+#    st.session_state.messages.append({"role": "assistant", "content": response})
 
-#template = template.replace('__MSG__', data1.replace('"', ''))
-template = template.replace('__MSG__', data1)
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o-mini"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-    st.session_state.messages = [{'role': 'system', 'content': template}]
-
-# それまでのメッセージを全て表示したままにする（このloopがないと、同じ場所を更新しながら会話が続く）
-#for message in st.session_state.messages[1:]:
-#    with st.chat_message(message["role"]):
-#        st.markdown(message["content"]) # 表示する（一瞬ですべて書き下す）
-
-# 入力されたら、内容をpromptに格納(入力までは待機)
-if prompt := st.chat_input("質問はありますか？"):
-    # messagesにユーザーのプロンプトを追加
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # ユーザーのアイコンで、promptをそのまま表示
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    # AIのアイコンで
-    with st.chat_message("assistant"):
-        # ChatGPTの返答をstreamに格納
-        stream = client.chat.completions.create(
-            model = st.session_state["openai_model"],
-            # 会話履歴をすべて参照して渡す
-            messages = [
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream = True,
-            temperature = 0.5,
-        )
-        # AIの返答を流れるように出力
-        response = st.write_stream(stream)
-    
-    # messagesにAIの返答を格納
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    #　ここで一旦終わり、入力待機となる
