21fa944ffdd9f23a8a0948664c98034dcfb958fe
aimathstats <aimathstats@gmail.com>
2024-08-11 22:08:45 +0900

    Update app4_autoprompt2.py

--- app4_autoprompt2.py
+++ app4_autoprompt2.py
@@ -1,18 +1,22 @@
-import openai
+from openai import OpenAI
 import streamlit as st
+import datetime
+import time
 
 # OpenAI APIキーを設定
-openai.api_key = "YOUR_API_KEY"
+#openai.api_key = "YOUR_API_KEY"
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def generate_response(prompt, conversation_history):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",  # GPT-4モデルを使用
+    response = client.chat.completions.create(
+    #response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
         messages=conversation_history + [{"role": "user", "content": prompt}],
         max_tokens=150,
         temperature=0.7,
         stop=None,
     )
-    return response.choices[0].message["content"]
+    return response.choices[0].message.content
 
 def chat_between_gpts():
     # 初期設定
@@ -21,22 +25,20 @@ def chat_between_gpts():
     current_turn = 1
 
     while current_turn <= max_turns:
-        # AI 1が質問を生成
+        # AI_1が質問を生成
         if current_turn == 1:
-            ai1_prompt = "Hello, can you ask me a question to start our conversation?"
+            ai1_prompt = "こんにちわ！会話を始めるために質問してくれる?"
         else:
             ai1_prompt = "Can you ask me another question based on our conversation?"
 
         ai1_response = generate_response(ai1_prompt, conversation_history)
         conversation_history.append({"role": "assistant", "content": ai1_response})
+        st.write(f"**AI-1:** {ai1_response}")
 
-        st.write(f"**GPT-1:** {ai1_response}")
-
-        # AI 2が質問に回答
+        # AI_2が質問に回答
         ai2_response = generate_response(ai1_response, conversation_history)
         conversation_history.append({"role": "assistant", "content": ai2_response})
-
-        st.write(f"**GPT-2:** {ai2_response}")
+        st.write(f"**AI-2:** {ai2_response}")
 
         current_turn += 1
 
