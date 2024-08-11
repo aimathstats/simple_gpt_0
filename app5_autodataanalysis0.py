3d4d47f92049bc56a0d120d9073023054d7fa7c1
aimathstats <aimathstats@gmail.com>
2024-08-11 16:00:49 +0900

    Update app5_autodataanalysis0.py

--- app5_autodataanalysis0.py
+++ app5_autodataanalysis0.py
@@ -68,26 +68,15 @@ def summary_wiki():
     __MSG__
     '''
     template = template.replace('__MSG__', data2.replace('"', ''))
-    
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-4o-mini"
-    
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        st.session_state.messages = [{'role': 'system', 'content': template}]
+    st.session_state.messages = []
     
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model = st.session_state["openai_model"],
-            messages = [
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+            model = "gpt-4o-mini",
+            messages = [{'role': 'system', 'content': template}],
             stream = True,
         )
         response = st.write_stream(stream)
-    #st.session_state.messages.append({"role": "assistant", "content": response})
-
 
 def pdf_plot_analysis_ai():
     # PDFからのテーブル取得と可視化：都道府県別コロナ定点観測の折れ線
